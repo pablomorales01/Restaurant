5bd71a25c36dde74c177e2e85796ded9c44682ca
Pablo Morales Alarcón <pamoral@alumnos.ubiobio.cl>
2014-11-03 03:50:27 -0300

    cambios en index

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -15,6 +15,7 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/jquery-ui-1.10.3.custom.css">
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
@@ -24,15 +25,18 @@
 <div class="container" id="page">
 <?php echo 'contenido main' ?>
 	<div id="header">
+		
+		<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logopenco.png" width="350" height="150">
 		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+		
 	</div><!-- header -->
 
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				//array('label'=>'Contact', 'url'=>array('/site/contact')),
 				array('label'=>'Administrar Usuarios'
 					, 'url'=>Yii::app()->user->ui->userManagementAdminUrl
 					, 'visible'=>!Yii::app()->user->isGuest),
@@ -64,7 +68,9 @@
 </div><!-- page -->
  <?php echo Yii::app()->user->ui->displayErrorConsole(); ?> <!--Indica los permisos que requiero-->
 
- <script src="<?php echo Yii::app()->request->baseUrl;?>/js/jquery.min.js"></script>
- <script src="<?php echo Yii::app()->request->baseUrl;?>/js/bootstrap.js"></script>
+ <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+ <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap.js" type="text/javascript"></script>
+ <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
+ <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/demo.js" type="text/javascript"></script>
 </body>
 </html>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,17 +4,102 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<!--<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>-->
+<div class="row">
+  <div class="col-xs-12 col-sm-2 col-md-3">	<!--Menú -->
+	<div class="clearfix">
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<ul id="menu" class="ui-menu ui-widget ui-widget-content ui-corner-all" role="menu" tabindex="0" aria-activedescendant="ui-id-18">
+	            <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-17" class="ui-corner-all" tabindex="-1" role="menuitem">Aberdeen</a></li>
+	            <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-18" class="ui-corner-all" tabindex="-1" role="menuitem">Ada</a></li>
+	            <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-19" class="ui-corner-all" tabindex="-1" role="menuitem">Adamsville</a></li>
+	            <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-20" class="ui-corner-all" tabindex="-1" role="menuitem">Addyston</a></li>
+	            <li class="ui-menu-item" role="presentation">
+	                <a href="#" aria-haspopup="true" id="ui-id-21" class="ui-corner-all" tabindex="-1" role="menuitem"><span class="ui-menu-icon ui-icon ui-icon-carat-1-e"></span>Delphi</a>
+	                <ul class="ui-menu ui-widget ui-widget-content ui-corner-all" role="menu" aria-expanded="false" style="display: none; top: 9826px; left: 212px;" aria-hidden="true">
+	                    <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-24" class="ui-corner-all" tabindex="-1" role="menuitem">Ada</a></li>
+	                    <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-25" class="ui-corner-all" tabindex="-1" role="menuitem">Saarland</a></li>
+	                    <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-26" class="ui-corner-all" tabindex="-1" role="menuitem">Salzburg</a></li>
+	                </ul>
+	            </li>
+	            <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-22" class="ui-corner-all" tabindex="-1" role="menuitem">Saarland</a></li>
+	            <li class="ui-menu-item" role="presentation">
+	                <a href="#" aria-haspopup="true" id="ui-id-23" class="ui-corner-all" tabindex="-1" role="menuitem"><span class="ui-menu-icon ui-icon ui-icon-carat-1-e"></span>Salzburg</a>
+	                <ul class="ui-menu ui-widget ui-widget-content ui-corner-all" role="menu" aria-hidden="true" aria-expanded="false" style="display: none;">
+	                    <li class="ui-menu-item" role="presentation">
+	                        <a href="#" aria-haspopup="true" id="ui-id-27" class="ui-corner-all" tabindex="-1" role="menuitem"><span class="ui-menu-icon ui-icon ui-icon-carat-1-e"></span>Delphi</a>
+	                        <ul class="ui-menu ui-widget ui-widget-content ui-corner-all" role="menu" aria-hidden="true" aria-expanded="false" style="display: none;">
+	                            <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-30" class="ui-corner-all" tabindex="-1" role="menuitem">Ada</a></li>
+	                            <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-31" class="ui-corner-all" tabindex="-1" role="menuitem">Saarland</a></li>
+	                            <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-32" class="ui-corner-all" tabindex="-1" role="menuitem">Salzburg</a></li>
+	                        </ul>
+	                    </li>
+	                    <li class="ui-menu-item" role="presentation">
+	                        <a href="?Delphi" aria-haspopup="true" id="ui-id-28" class="ui-corner-all" tabindex="-1" role="menuitem"><span class="ui-menu-icon ui-icon ui-icon-carat-1-e"></span>Delphi</a>
+	                        <ul class="ui-menu ui-widget ui-widget-content ui-corner-all" role="menu" aria-hidden="true" aria-expanded="false" style="display: none;">
+	                            <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-33" class="ui-corner-all" tabindex="-1" role="menuitem">Ada</a></li>
+	                            <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-34" class="ui-corner-all" tabindex="-1" role="menuitem">Saarland</a></li>
+	                            <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-35" class="ui-corner-all" tabindex="-1" role="menuitem">Salzburg</a></li>
+	                        </ul>
+	                    </li>
+	                    <li class="ui-menu-item" role="presentation"><a href="#" id="ui-id-29" class="ui-corner-all" tabindex="-1" role="menuitem">Perch</a></li>
+	                </ul>
+	            </li>
+	        </ul>
+	    </div>
+	    <!--FIN Menú -->
+	    </div>
+  <div class="col-xs-12 col-sm-8 col-md-8"><section>
+	    
+	    <article>
+	    <!--Slides-->
+	    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		  </ol>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner">
+		    <div class="item active">
+		      <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/comida.jpg" class="img-responsive" alt="Responsive image"> <!-- alt = texto-->
+		      <div class="carousel-caption">
+		        ...
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/penco.jpg" class="img-responsive" alt="Responsive image">
+		      <div class="carousel-caption">
+		        ...
+		      </div>
+		    </div>
+		    ...
+		    <!--<div class="item active">
+		      <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logopenco.jpg" class="img-responsive" alt="Responsive image">
+		      <div class="carousel-caption">
+		        ...
+		      </div>
+		    </div>-->
+		  </div>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left"></span>
+		  </a>
+		  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right"></span>
+		  </a>
+		</div>
+		</article>
+	    <!--FIN Slides-->
+	    </section>
+	    </div>
+</div>
+
+	    
+
+	    
+
+    </div>

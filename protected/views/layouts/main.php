6bc2c6ddf3d7e0bca10cb52c334446ae04987984
Pablo Morales Alarcón <pamoral@alumnos.ubiobio.cl>
2014-11-04 03:11:52 -0300

    nuevo menu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -49,6 +49,126 @@
 			),
 		)); ?>
 	</div><!-- mainmenu -->
+
+	<!--Nuevo Menú-->
+
+	<?php
+$this->widget('bootstrap.widgets.BsNavbar', array(
+    'collapse' => true,
+    'brandLabel' => BsHtml::icon(BsHtml::GLYPHICON_HOME),
+    'brandUrl' => Yii::app()->homeUrl,
+    'items' => array(
+        array(
+            'class' => 'bootstrap.widgets.BsNav',
+            'type' => 'navbar',
+            'activateParents' => true,
+            'items' => array(
+
+            	//DEL BOTON DROPDOWN
+                array(
+                    'label' => 'Home',
+                    'url' => array(
+                        '/site/index'
+                    ),
+                    'items' => array(
+                        BsHtml::menuHeader(BsHtml::icon(BsHtml::GLYPHICON_BOOKMARK), array(
+                            'class' => 'text-center',
+                            'style' => 'color:#99cc32;font-size:32px;'
+                        )),
+                        array(
+                            'label' => 'Home',
+                            'url' => array(
+                                '/site/index'
+                            )
+                        ),
+                        array(
+                            'label' => 'About',
+                            'url' => array(
+                                '/site/page',
+                                'view' => 'about'
+                            )
+                        ),
+                        array(
+                            'label' => 'Contact',
+                            'url' => array(
+                                '/site/contact'
+                            )
+                        ),
+                        BsHtml::menuDivider(),
+                        array(
+                            'label' => 'Login',
+                            'url' => array(
+                                '/site/login'
+                            ),
+                            'visible' => Yii::app()->user->isGuest,
+                            'icon' => BsHtml::GLYPHICON_LOG_IN
+                        ),
+                        array(
+                            'label' => 'Logout (' . Yii::app()->user->name . ')',
+                            'url' => array(
+                                '/site/logout'
+                            ),
+                            'visible' => !Yii::app()->user->isGuest
+                        ),
+                        array(
+                            'label' => 'Home',
+                            'url' => array(
+                                '/site/index'
+                            ),
+                            'icon' => BsHtml::GLYPHICON_HOME
+                        ),
+                        array(
+                            'label' => 'About',
+                            'url' => array(
+                                '/site/page',
+                                'view' => 'about'
+                            ),
+                            'icon' => BsHtml::GLYPHICON_PAPERCLIP
+                        ),
+                        array(
+                            'label' => 'Contact',
+                            'url' => array(
+                                '/site/contact'
+                            ),
+                            'icon' => BsHtml::GLYPHICON_FLOPPY_OPEN
+                        )
+                    )
+                )
+            )
+			//FIN BOTON DROPDOWN
+        ),
+        array(
+            'class' => 'bootstrap.widgets.BsNav',
+            'type' => 'navbar',
+            'activateParents' => true,
+            'items' => array(
+                array('label'=>'Administrar Usuarios'
+					, 'url'=>Yii::app()->user->ui->userManagementAdminUrl
+					, 'visible'=>!Yii::app()->user->isGuest),
+                array('label'=>'Login'
+					, 'url'=>Yii::app()->user->ui->loginUrl
+					, 
+
+                    'pull' => BsHtml::NAVBAR_NAV_PULL_RIGHT,
+                    'visible' => Yii::app()->user->isGuest
+                ),
+                array(
+                    'label' => 'Logout (' . Yii::app()->user->name . ')',
+                    'pull' => BsHtml::NAVBAR_NAV_PULL_RIGHT,
+                    'url'=>Yii::app()->user->ui->logoutUrl,
+                    'visible' => !Yii::app()->user->isGuest
+                )
+            ),
+            'htmlOptions' => array(
+                'pull' => BsHtml::NAVBAR_NAV_PULL_RIGHT
+            )
+        )
+        
+    )
+));
+?>
+
+	<!--Fin Nuevo Menú-->
 	<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 			'links'=>$this->breadcrumbs,
